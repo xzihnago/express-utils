ff4eaddbf944b680c2bbb879f7eb781f06f210bc
xzihnago <xzihnago@gmail.com>
2025-03-17 22:45:53 +0800

    Refactor middleware

--- src/middleware/errorHandler/www.ts
+++ src/middleware/errorHandler/www.ts
@@ -5,27 +5,17 @@ export const www: MiddlewareError<
   ]
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
 > = (redirect, messages) => (error, _req, res, _next) => {
-  let url: string | undefined;
-  if (redirect && typeof redirect === "object") {
-    url = redirect[res.statusCode];
-  }
+  const url =
+    typeof redirect === "string" ? redirect : redirect?.[res.statusCode];
 
-  let message: string | undefined;
-  if (messages) {
-    if (typeof messages === "string") {
-      message = messages;
-    } else {
-      message = messages[res.statusCode];
-    }
-  } else if (error instanceof Error) {
-    message = error.message;
-  }
-  message ??= "Unknown error";
+  const message =
+    (typeof messages === "string" ? messages : messages?.[res.statusCode]) ??
+    (error instanceof Error ? error.message : "Unknown error");
 
   res.send(`
-      <script>
-        alert("${message}");
-        ${url ? `window.location.replace("${url}")` : "history.back()"}
-      </script>
-    `);
+    <script>
+      alert("${message}");
+      ${url ? `window.location.replace("${url}")` : "history.back()"}
+    </script>
+  `);
 };
--- src/middleware/validateSchema/superstruct.ts
+++ src/middleware/validateSchema/superstruct.ts
@@ -1,9 +1,9 @@
-import { create, type Struct } from "superstruct";
+import type { Struct } from "superstruct";
 
 export const superstruct: Middleware<[schema: Struct]> =
   (schema) => (req, res, next) => {
     try {
-      req.body = create(req.body, schema);
+      req.body = schema.create(req.body);
     } catch (error) {
       res.status(422);
       throw error;
