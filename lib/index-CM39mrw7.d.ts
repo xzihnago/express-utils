36cf1e02389261cdbdb7d1ec2af56e42f690ee97
xzihnago <xzihnago@gmail.com>
2025-05-14 20:00:37 +0800

    Update 0.5.0

--- lib/index-CM39mrw7.d.ts
+++ lib/index-CM39mrw7.d.ts
@@ -1,13 +1,13 @@
 import { routeLogger } from './middleware/routeLogger.js';
 import { responseHandler } from './middleware/responseHandler.js';
 import { i as index$2 } from './index-D_L86mT5.js';
-import { i as index$1 } from './index-CTVeQFb_.js';
-import { i as index$3 } from './index-Bdlz2-nR.js';
+import { i as index$1 } from './index-CEYXosCm.js';
+import { i as index$3 } from './index-D8Z-fIvx.js';
 
 declare const index_responseHandler: typeof responseHandler;
 declare const index_routeLogger: typeof routeLogger;
 declare namespace index {
-  export { index$1 as authentication, index$2 as errorHandler, index_responseHandler as responseHandler, index_routeLogger as routeLogger, index$3 as validateSchema };
+  export { index$1 as authentication, index$2 as errorHandler, index_responseHandler as responseHandler, index_routeLogger as routeLogger, index$3 as schemas };
 }
 
 export { index as i };
--- lib/middleware/authentication/index.d.ts
+++ lib/middleware/authentication/index.d.ts
@@ -1,3 +1,1 @@
 export { local } from './local.js';
-export { jwt } from './jwt.js';
-import 'jsonwebtoken';
